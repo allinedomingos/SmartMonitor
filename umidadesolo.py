8fc89b3f03c47ff492338e84a8b024ecc5265363
Alline Domingos <lline.domingosdmg@gmail.com>
2019-10-03 23:20:39 -0300

    Create umidadesolo.py

---
+++ umidadesolo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+ 
+# Esta é nossa função de callback. Ela vai ser chamada toda vez que uma mudança for detectada no pino GPIO correspondente.
+def callback(channel):
+    if GPIO.input(channel):
+        print('Umido')
+    else:
+        print('Seco')
+ 
+ 
+# Set things up
+channel = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+ 
+# Vamos cadastrar a função de callback para o nosso pino
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+ 
+# Loop infinito para manter o script rodando
+while True:
